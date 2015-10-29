30b449c0e641c644c51639db083dc28a8de57bcd
moxpower <maxkohl@gmx.de>
2015-10-29 23:44:30 +0100

    temps

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
+#http://localhost:5000
+
+from flask import render_template
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+    
+@app.route('/index')
+def index():
+    user = {'nickname':'moxpower'}
+    return render_template('index.html',
+                           title='Home',
+                           user=user)
 
 if __name__ == '__main__':
     app.run()
